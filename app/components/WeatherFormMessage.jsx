110fa43d7ae4a2a45f17cbae689abf31cb8c7ee0
jamestothe <jamesdelacruz@gmail.com>
2016-11-03 16:15:57 -0700

    refactored components into stateless functional components

--- app/components/WeatherFormMessage.jsx
+++ app/components/WeatherFormMessage.jsx
@@ -1,14 +1,12 @@
 var React = require('react');
 
-var WeatherFormMessage = React.createClass({
-  render: function () {
-    var {location, temp} = this.props;
-    return (
-      <div>
-        <p> It's {temp} degrees in {location} right now. </p>
-      </div>
-    );
-  }
-})
+var WeatherFormMessage = (props) => {
+  var {location, temp} = props;
+  return (
+    <div>
+      <p> It's {temp} degrees in {location} right now. </p>
+    </div>
+  );
+};
 
 module.exports = WeatherFormMessage;
